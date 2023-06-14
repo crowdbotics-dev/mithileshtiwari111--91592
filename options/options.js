034749ba8721df056cac2ae46ea17230b58261ef
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-06-14 23:49:37 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "mithileshtiwari111--91592",
   url: "https://mithileshtiwari111--91592.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
